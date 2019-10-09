a150325c77b7c44c1a18b3205b7b73aae6d74a7e
naapak <nakkarapaka@gmail.com>
2019-10-08 22:58:58 -0400

    shop models and item models created

---
+++ server/schemas/shop.js
@@ -0,0 +1,25 @@
+let mongoose = require("mongoose");
+
+const itemSchema = new mongoose.Schema({
+  Title: { type: String },
+  price: { type: Number },
+  description: { type: String },
+  category: { type: String },
+  photos: [{ type: String }]
+});
+
+let shopSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
+  title: {
+    type: String
+  },
+  description: {
+    type: String
+  },
+  cover_photo: {
+    type: String
+  },
+  items: [itemSchema]
+});
+
+module.exports = mongoose.model("shop", shopSchema);
