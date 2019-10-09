7d3410abb338d1631d1de4dac25f42d407a14967
dbertozzi <david.a.bertozzi@gmail.com>
2019-10-08 22:34:22 -0400

    First commit
    Added MyShop to router.

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import { theme } from "./themes/theme";
 import LandingPage from "./pages/Landing";
 import Navbar from "./components/navbar/navbar";
 import SignAll from "./pages/SignAll";
+import MyShop from "./pages/MyShop";
 import "./App.css";
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
           <Route exact path="/" component={LandingPage} />
           <Route path="/signin" component={SignAll} />
           <Route path="/signup" component={SignAll} />
+          <Route path="/myshop" component={MyShop} />
         </Switch>
       </BrowserRouter>
     </MuiThemeProvider>
