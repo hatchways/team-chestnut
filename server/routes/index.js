fa0d4bf7712a5318edbabbf7feb84d196528f928
naapak <nakkarapaka@gmail.com>
2019-10-11 11:50:16 -0400

    cleaned jwt and gets user details

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,36 +2,31 @@ var express = require("express");
 var router = express.Router();
 const User = require("../schemas/users");
 const jwt = require("jsonwebtoken");
+const verify = require("./verify-token");
 
 router.get("/welcome", function(req, res, next) {
   res.status(200).send({ welcomeMessage: "Step 1 (completed)" });
 });
 
 //get user details along with shop..
-router.get("/users/:userid", async (req, res, next) => {
+router.get("/users/:userid", verify, async (req, res, next) => {
   var userid = req.params.userid;
   const token = req.header("auth-token");
 
-  if (!token) return res.status(401).send("Access Denied");
+  verify;
 
-  try {
-    jwt.verify(token, process.env.TOKEN_SECRET);
+  const user = await User.findOne(
+    { _id: userid },
+    { __v: false, _id: false, password: false, date: false }
+  ).populate("shop");
 
-    const user = await User.findOne(
-      { _id: userid },
-      { __v: false, _id: false, password: false, date: false }
-    ).populate("shop");
+  if (!user) return res.status(400).send({ message: "User is not found..." });
+  console.log("user is:", user);
 
-    if (!user) return res.status(400).send({ message: "User is not found..." });
-    console.log("user is:", user);
-
-    res
-      .header("auth-token", token)
-      .status(200)
-      .send({ user });
-  } catch (err) {
-    res.status(400).send("Invalid Token");
-  }
+  res
+    .header("auth-token", token)
+    .status(200)
+    .send({ user });
 });
 
 module.exports = router;
--- server/routes/verify-token.js
+++ server/routes/verify-token.js
@@ -1,16 +1,13 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-module.exports = function (req, res, next) {
-    const token = req.header('auth-token');
-    if (!token) return res.status(401).send('Access Denied');
-    console.log(token);
-    console.log(process.env.SECRET_TOKEN)
+module.exports = function(req, res, next) {
+  const token = req.header("auth-token");
+  if (!token) res.status(401).send("Access Denied");
 
-    try {
-        const verified = jwt.verify(token, process.env.TOKEN_SECRET);
-        req.user = verified;
-        next();
-    } catch (err) {
-        res.status(400).send('Invalid Token');
-    }
-}
+  try {
+    jwt.verify(token, process.env.TOKEN_SECRET);
+    next();
+  } catch (err) {
+    res.status(400).send("Invalid Token");
+  }
+};
