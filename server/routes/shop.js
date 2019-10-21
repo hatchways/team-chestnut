071657949607d0326a06dbd079c3b54a4dd98e6b
naapak <nakkarapaka@gmail.com>
2019-10-21 03:35:02 -0400

    single image delete

--- server/routes/shop.js
+++ server/routes/shop.js
@@ -24,29 +24,66 @@ router.post("/image-upload", verify, function(req, res) {
 router.post("/item/:itemid", multipleUpload, verify, async (req, res, next) => {
   const itemid = req.params.itemid;
   const token = req.header("auth-token");
+  // need to authenticate the user authorization to the shop
+
   const item = await items.findById(itemid, { __v: false });
 
   if (!item) return res.status(400).send({ message: "Item not found..." });
 
   if (req.files) {
-    req.files.forEach(file => {     
+    req.files.forEach(file => {
       if (item.photos.indexOf(file.location) < 0) {
         item.photos.push(file.location);
-      } 
+      }
     });
   }
-    item.title = req.body.title;
-    item.price = req.body.price;
-    item.description = req.body.description;
-    item.category = req.body.category;
+  item.title = req.body.title ? req.body.title : item.title;
+  item.price = req.body.price ? req.body.price : item.price;
+  item.description = req.body.description
+    ? req.body.description
+    : item.description;
+  item.category = req.body.category ? req.body.category : item.category;
 
-    const newItem = await item.save();
+  const newItem = await item.save();
 
-    res
-      .header("auth-token", token)
-      .status(200)
-      .send({ item: newItem });
+  return res
+    .header("auth-token", token)
+    .status(200)
+    .send({ item: newItem });
+});
+
+router.post("/item/delete/:itemid", verify, async (req, res, next) => {
+  const itemid = req.params.itemid;
+  const token = req.header("auth-token");
+  const imageUrl = req.body.image;
+  const key = imageUrl.split('/').slice(-1)[0];
+  const response = deleteImage(key); 
+  const item = await items.findById(itemid, { __v: false });
+
+response.then((data,rej) =>{
+
+  if(data.Deleted[0].Key === key) {
+    item.photos = item.photos.filter(url => {
+        return url !== imageUrl;
+      });
+    item.save().then((response,error)=>{
+      if(response){
+        return res.header("auth-token", token).status(200).send({ item: response });
+      } else if (error){
+        return res.status(400).send({ message: error.message });
+      }
+    });
+       
+  } else if (rej){
+    return res.status(400).send({ message: "There was an error trying to delete the image" });
+  } else {
+
+    res.status(400).send({ message: "Image could not be deleted" });
+  }
   
+
+});
+
 });
 
 module.exports = router;
--- server/services/file-delete.js
+++ server/services/file-delete.js
@@ -7,22 +7,31 @@ aws.config.update({
   secretAccessKey: process.env.IAM_USER_SECRET,
   region: process.env.BUCKET_REGION
 });
+
 const s3 = new aws.S3();
 
-function deleteImage(keysAll){ 
-    
+function deleteImage (keysAll) {
+    return new Promise(function (resolve, reject) {
     var params = {
         Bucket: process.env.BUCKET_NAME, 
         Delete: { 
-          Objects: keysAll // keysAll is array of objects with key: image.jpg
+          Objects: [ { Key: keysAll}] // keysAll is array of objects with key: image.jpg
         },
       };
     
-    s3.deleteObjects(params, function(err, data) {
-    if (err) console.log(err, err.stack); // an error occurred
-    else     return data          // successful response
-  });
+        s3.deleteObjects(params,function (err, data) {
+        if (err) {
+            console.log('the error  is ', err); 
+            reject(err)
+        }
+        else {
+            console.log('the delete data is ', data);  
+            resolve(data)
+        }           
+      });
+    
 
-}
+    });
+  };
 
 module.exports = deleteImage;
